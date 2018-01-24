d99772b7f57e21537305bddcdfd5097de35b11e2
Lars Emblem <lars.emblem@gmail.com>
2018-01-24 18:22:47 +0100

    Initial commit

---
+++ basics.go
@@ -0,0 +1,17 @@
+// some basics in go programming
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func foo() {
+	fmt.Println("The square root of 4 is", math.Sqrt(4))
+}
+
+func main() {
+	foo()
+	fmt.Println("A number form 1-100 =>", rand.Intn(100))
+}
---
+++ function.go
@@ -0,0 +1,9 @@
+package main
+
+func multiple(a, b string) (string, string) {
+	return a, b
+}
+
+func main() {
+	w1, w2 := "Hey", "there"
+}
